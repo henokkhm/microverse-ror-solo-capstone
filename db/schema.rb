ac1d2f493b508ac196954da13c55b1d31884f2ff
Henok Kirubel <henokhm2@gmail.com>
2023-11-24 15:09:56 +0300

    Require username to not be null

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_24_110544) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
