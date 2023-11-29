551f1cf620187d04902bcf5c7c7c989900225cce
Henok Kirubel <henokhm2@gmail.com>
2023-11-30 02:25:21 +0300

    Allow name form field for sign up endpoint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  def after_sign_up_path_for(resource)
+    if resource.errors.empty?
+      super(resource)
+    else
+      # Redirect to the registration page with errors displayed
+      new_user_registration_path
+    end
+  end
 end
