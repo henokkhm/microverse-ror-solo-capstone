89cba3f841f506675fcb48efca4a4afe9da9a6e9
Henok Kirubel <henokhm2@gmail.com>
2023-11-30 00:02:59 +0300

    Add Expenses model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_24_121141) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_29_205507) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,15 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_24_121141) do
     t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
+  create_table "expenses", force: :cascade do |t|
+    t.string "name"
+    t.decimal "amount"
+    t.bigint "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_expenses_on_author_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
@@ -30,4 +39,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_24_121141) do
   end
 
   add_foreign_key "categories", "users"
+  add_foreign_key "expenses", "users", column: "author_id"
 end
